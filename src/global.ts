a0644d90c9172b6cf455e1fe2c4fd57dd6d74715
caffxxnx <caffxxnx@gmail.com>
2025-08-07 17:43:13 +0900

    refactor: update global file path & contents

--- src/global.ts
+++ src/global.ts
@@ -1,11 +1,11 @@
 import type { GlobalSettingVariableType } from '@/types/global.d.ts';
 
 const GLOBAL_SETTING_VARIABLE: GlobalSettingVariableType = {
-  RESERVATION_AVAILABLE_DAYS: 180,
+  RESERVATION_AVAILABLE_DAYS: 10,
   SERVICE_START_TIME: '10:00',
   SERVICE_END_TIME: '18:00',
-  INTERVAL_MINUTES: 15,
-  NUM_PER_INTERVAL: 15,
+  INTERVAL_MINUTES: 60,
+  NUM_PER_INTERVAL: 3,
   LATEST_AVAILABLE_HOUR: 'next-day',
 };
 
