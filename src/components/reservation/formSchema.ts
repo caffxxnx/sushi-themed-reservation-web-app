a9db8d1ead42ca1afc32e0248dbb6a8f5616c05b
caffxxnx <caffxxnx@gmail.com>
2025-08-13 12:18:03 +0900

    feat: add Form validations
    - phone number validation
    - number of guests validation

--- src/components/reservation/formSchema.ts
+++ src/components/reservation/formSchema.ts
@@ -4,10 +4,16 @@ export const formSchema = z.object({
   date: z.string().min(1, { message: 'Date is required' }),
   time: z.string({ message: 'Time is required' }),
   name: z.string().nonempty({ message: 'Name is required' }),
-  phone: z.string().nonempty({ message: 'Phone is required' }),
-  guests: z
+  phone: z
     .string()
-    .refine((n) => +n > 0, { message: 'Number is required' }),
+    .nonempty({ message: 'Phone is required' })
+    .regex(/^0[789]0\d{8}$/, {
+      message:
+        'Phone number must be a valid 11-digit number starting with 070, 080, or 090',
+    }),
+  guests: z.string().refine((n) => +n > 0 && +n <= 10, {
+    message: 'Number of guests must be 1-10',
+  }),
 });
 
 export type FormValues = z.infer<typeof formSchema>;
